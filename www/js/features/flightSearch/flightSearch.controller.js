ef69b94a124d848ae93f153d61802ca15591a30f
NAO <naokosaka17@gmail.com>
2017-03-20 18:42:46 -0400

    make server.js

--- www/js/features/flightSearch/flightSearch.controller.js
+++ www/js/features/flightSearch/flightSearch.controller.js
@@ -2,9 +2,15 @@ angular
   .module('flightManagement.flightSearch')
   .controller('FlightSearchCtrl', FlightSearchCtrl);
 
-function FlightSearchCtrl() {
+function FlightSearchCtrl($http) {
   var vm = this;
+  function getFlight(){
 
+    return $http.get('/api/v1/themes', function(req,res) {
+    sabreCall('/v1/lists/supported/shop/themes', res);
+    });
+  }
+}
 
 
 }
