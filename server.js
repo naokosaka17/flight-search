57f2c0a528fc95f19e8368eb73ab828352e983c3
NAO <naokosaka17@gmail.com>
2017-03-20 22:13:22 -0400

    added server.js and config folder

--- server.js
+++ server.js
@@ -1,96 +1,13 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    morgan = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    compression = require('compression'),
-    errorhandler = require('errorhandler'),
-    multer  = require('multer');
+(function () {
+  'use strict';
+  var express = require('express');
+  var app = express();
 
-var app = express();
+  require('./config/config_app.js')(app);
+  require('./config/config_routes.js')(app);
 
 
-
-module.exports = function(app) {
-  app.use(compression());
-  app.use(morgan('dev'));
-  app.use(bodyParser());
-  app.use(multer());
-  app.use(methodOverride());
-  app.use(express.static(__dirname + '/../www/'));
-
-  // app.use(favicon(__dirname + '/../../Twyst-Web-Apps/common/images/favicon/twyst.ico'));
-  app.all("/api/*", function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, Accept");
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, HEAD, DELETE, OPTIONS");
-      return next();
-  });
-
-  app.use(errorhandler({
-      dumpExceptions: true,
-      showStack: true
-  }));
-
-};
-
-var SabreDevStudio = require('sabre-dev-studio');
-var sabreDevStudio = new SabreDevStudio({
-  client_id:     'V1:8wdlxlxzwck14mlh:DEVCENTER:EXT',
-  client_secret: 'lV65yKhM',
-  uri:           'https://api.test.sabre.com'
-});
-var options = {};
-
-module.exports = function(app) {
-  app.get('/api/v1/themes', function(req,res) {
-    sabreCall('/v1/lists/supported/shop/themes', res);
-  });
-
-  app.get('/api/v1/routes', function(req,res) {
-    sabreCall('/v1/shop/flights/fares?origin=CLT&departuredate=2017-04-01&returndate=2017-04-01', res);
-  });
-
-  app.get('/api/v1/top', function(req,res) {
-    sabreCall('/v1/lists/top/destinations?origin=NYC&lookbackweeks=8&topdestinations=5', res);
-  });
-
-  app.get('/api/v1/cities', function(req,res) {
-    sabreCall('/v1/lists/supported/cities', res);
-  });
-
-  app.get('/api/v1/places', function(req,res) {
-    sabreCall('/v1/shop/flights/fares?origin=' + req.query.origin +
-    '&departuredate=' + req.query.departuredate +
-    '&returndate=' + req.query.returndate +
-    '&maxfare=' + req.query.maxfare, res);
-  });
-
-};
-
-function sabreCall(q, res) {
-  sabreDevStudio.get(q, options, function(err, data) {
-    response(res, err, data);
-  });
-}
-
-function response(res, err, data) {
-  if (err) {
-    res.status(200).send({
-      'status': false,
-      'message': 'Error',
-      'info': err
-    });
-  } else {
-    res.status(200).send({
-      'status': true,
-      'message': 'Success',
-      'info': data
-    });
-  }
-}
-
-// START THE SERVER
+  // START THE SERVER
   console.log('STARTING THE SABRE SERVER');
   console.log('-------------------------');
   app.listen(3000);
@@ -99,3 +16,5 @@ function response(res, err, data) {
       console.log(error.stack);
       console.log(error);
   });
+
+})();
